d6aad49dd49fac2408be13270ee42b3996c4ad64
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-15 00:12:39 +0600

    project End

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,27 +1,66 @@
-function handelKeybordButtonPress(event){
-    const playerPressed = event.key;
-    const curretnAlphabateElement = document.getElementById('current-alphabate');
-    const currentAlphabate = curretnAlphabateElement.innerText;
-    const expectedAlphabate = currentAlphabate.toLowerCase();
-
-    if(playerPressed === expectedAlphabate){
-        removeBackgroundColorById(playerPressed);
-        continueGame();
-    }else{
-        console.log('You Missed. You Lose your life')
-    }
+function handelKeybordButtonPress(event) {
+  const playerPressed = event.key;
+//   game over
+if(playerPressed === 'Escape'){
+    gameOver();
 }
-document.addEventListener('keyup',handelKeybordButtonPress);
+  const curretnAlphabateElement = document.getElementById("current-alphabate");
+  const currentAlphabate = curretnAlphabateElement.innerText;
+  const expectedAlphabate = currentAlphabate.toLowerCase();
+  let finalScore = 0;
+  if (playerPressed === expectedAlphabate) {
 
-function continueGame(){
-    const alphabate = getARandomAlphabate();
-    const currentAlphabante = document.getElementById('current-alphabate');
-    currentAlphabante.innerText = alphabate;
-    setBackgroundColorById(alphabate)
-}
+    const currentScore = getTextElementValueById('current-score');
+    const updateScore = currentScore + 1;
+    setTextElementValueById('current-score',updateScore);
+    // const currentScoreElement = document.getElementById("current-score");
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+    // const newScore = currentScore + 1;
+    // currentScoreElement.innerText = newScore;
 
-function play(){
-    hiddenElementById('home-screen');
-    showElementById('play-ground')
+    removeBackgroundColorById(playerPressed);
     continueGame();
+  } else {
+    const currentLifeScore = getTextElementValueById('current-life');
+    const updateLifeScore = currentLifeScore - 1;
+    setTextElementValueById('current-life',updateLifeScore);
+    // const currentLifeElement = document.getElementById("current-life");
+    // const currentLifeText = currentLifeElement.innerText;
+    // const currentLife = parseInt(currentLifeText);
+    // const newLife = currentLife - 1;
+    // currentLifeElement.innerHTML = newLife;
+
+    if (updateLifeScore === 0) {
+      gameOver();
+    }
+  }
+}
+document.addEventListener("keyup", handelKeybordButtonPress);
+
+function continueGame() {
+  const alphabate = getARandomAlphabate();
+  const currentAlphabante = document.getElementById("current-alphabate");
+  currentAlphabante.innerText = alphabate;
+  setBackgroundColorById(alphabate);
+}
+
+function play() {
+  hiddenElementById("home-screen");
+  hiddenElementById("score");
+  showElementById("play-ground");
+  setTextElementValueById('current-score',0)
+  setTextElementValueById('current-life',5)
+  continueGame();
+}
+
+function gameOver() {
+  hiddenElementById("play-ground");
+  showElementById("score");
+
+  const lastScore = getTextElementValueById('current-score');
+  setTextElementValueById('game-score',lastScore);
+
+  const currentAlphabate = getElementTextById('current-alphabate');
+  removeBackgroundColorById(currentAlphabate);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -17,6 +17,24 @@ function removeBackgroundColorById(elementId){
     element.classList.remove('bg-orange-400');
 }
 
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerHTML;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 function getARandomAlphabate(){
     const alphabateString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabates = alphabateString.split('');
